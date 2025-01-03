20b2eabec12197f67534daeb6b68b5c4bb40e236
exaggeratedrumors <exaggeratedrumors@gmail.com>
2025-01-03 16:38:21 +0100

    settings preferences

--- app/src/main/java/com/ertools/demooder/presentation/components/NavBar.kt
+++ app/src/main/java/com/ertools/demooder/presentation/components/NavBar.kt
@@ -11,14 +11,12 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.ertools.demooder.presentation.navigation.NavigationItem
 
 @Composable
