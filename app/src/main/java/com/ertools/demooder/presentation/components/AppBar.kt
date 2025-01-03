757906937abfbb636530545c1b5dffbb881a4801
exaggeratedrumors <exaggeratedrumors@gmail.com>
2025-01-03 16:38:21 +0100

    settings preferences

--- app/src/main/java/com/ertools/demooder/presentation/components/AppBar.kt
+++ app/src/main/java/com/ertools/demooder/presentation/components/AppBar.kt
@@ -1,21 +1,36 @@
 package com.ertools.demooder.presentation.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.ertools.demooder.R
 import com.ertools.demooder.presentation.navigation.NavigationItem
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,7 +40,7 @@ fun AppBar(
     scrollBehavior: TopAppBarScrollBehavior,
     menuItems: List<MenuItem>
 ) {
-    var menuExpanded by remember { mutableStateOf(false) }
+    val menuExpanded = remember { mutableStateOf(false) }
 
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -44,16 +59,41 @@ fun AppBar(
             )
         },
         actions = {
+            IconButton(onClick = { menuExpanded.value = true }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Menu",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
             DropdownMenu(
-                expanded = menuExpanded,
-                onDismissRequest = { menuExpanded = false }
+                expanded = menuExpanded.value,
+                onDismissRequest = { menuExpanded.value = false },
+                containerColor = MaterialTheme.colorScheme.surface,
+                border = BorderStroke(
+                    dimensionResource(id = R.dimen.global_dropdown_corner),
+                    MaterialTheme.colorScheme.surface
+                ),
+                modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+                shadowElevation = 0.dp,
+                tonalElevation = 0.dp
             ) {
                 menuItems.forEach { item ->
                     DropdownMenuItem(
                         text = {
-                            Text(item.text)
+                            Text(text = item.text)
                         },
                         onClick = { item.onClick(Unit) },
+                        modifier = Modifier.fillMaxHeight(),
+                        colors = MenuItemColors(
+                            textColor = MaterialTheme.colorScheme.onSurface,
+                            leadingIconColor = MaterialTheme.colorScheme.onSurface,
+                            trailingIconColor = MaterialTheme.colorScheme.onSurface,
+                            disabledTextColor = MaterialTheme.colorScheme.onTertiary,
+                            disabledLeadingIconColor = MaterialTheme.colorScheme.onTertiary,
+                            disabledTrailingIconColor = MaterialTheme.colorScheme.onTertiary
+                        )
+
                     )
                 }
             }
