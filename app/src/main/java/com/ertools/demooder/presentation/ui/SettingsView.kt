b6c3b36874c8046ac5f291f68efe279eb480fbd7
exaggeratedrumors <exaggeratedrumors@gmail.com>
2025-02-24 15:15:36 +0100

    refactor settings

--- app/src/main/java/com/ertools/demooder/presentation/ui/SettingsView.kt
+++ app/src/main/java/com/ertools/demooder/presentation/ui/SettingsView.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -15,42 +18,44 @@ import com.ertools.demooder.R
 import com.ertools.demooder.presentation.components.OptionData
 import com.ertools.demooder.presentation.components.ReturnScaffold
 import com.ertools.demooder.presentation.viewmodel.SettingsViewModel
-import com.ertools.demooder.utils.AppConstants
 import com.ertools.demooder.utils.Validation
+import kotlinx.coroutines.flow.first
 
 @Composable
 fun SettingsView(
     navController: NavHostController,
     settingsViewModel: SettingsViewModel = viewModel()
 ) {
-    val data = listOf(
-        OptionData.InputOptionData(
-            optionTitle = "Device damping",
-            defaultValue = AppConstants.SETTINGS_DEFAULT_DEVICE_DAMPING,
-            onValidate = { Validation.isNumberInRange(it, -100, 100) },
-            onSave = {
-                settingsViewModel.saveDeviceDamping(it.currentValue.value)
-            }
-        ),
-        OptionData.InputOptionData(
-            optionTitle = "Signal detection period in seconds",
-            defaultValue = AppConstants.SETTINGS_DEFAULT_SIGNAL_DETECTION_PERIOD,
-            onValidate = { Validation.isNumberInRange(it, 1, 10) },
-            onSave = { settingsViewModel.saveSignalDetectionPeriod(it.currentValue.value) }
-        ),
-        OptionData.SwitchOptionData(
-            optionTitle = "Enable notifications",
-            defaultValue = AppConstants.SETTINGS_DEFAULT_ENABLE_NOTIFICATIONS,
-            onSave = { settingsViewModel.saveEnableNotifications(it.currentValue.value) }
-        ),
-        OptionData.InputOptionData(
-            optionTitle = "Anger detection time in seconds before trigger notification",
-            defaultValue = AppConstants.SETTINGS_DEFAULT_ANGER_DETECTION_TIME,
-            onValidate = { Validation.isNumberInRange(it, 1, 60) },
-            onSave = { settingsViewModel.saveAngerDetectionTime(it.currentValue.value) }
+    val data = remember { mutableStateOf(emptyList<OptionData>()) }
+    LaunchedEffect(true) {
+        data.value = listOf(
+            OptionData.InputOptionData(
+                optionTitle = "Device damping",
+                defaultValue = settingsViewModel.deviceDamping.first(),
+                onValidate = { Validation.isNumberInRange(it, -100, 100) },
+                onSave = {
+                    settingsViewModel.saveDeviceDamping(it.currentValue.value)
+                }
+            ),
+            OptionData.InputOptionData(
+                optionTitle = "Signal detection period in seconds",
+                defaultValue = settingsViewModel.signalDetectionPeriod.first(),
+                onValidate = { Validation.isNumberInRange(it, 1, 10) },
+                onSave = { settingsViewModel.saveSignalDetectionPeriod(it.currentValue.value) }
+            ),
+            OptionData.SwitchOptionData(
+                optionTitle = "Enable notifications",
+                defaultValue = settingsViewModel.enableNotifications.first(),
+                onSave = { settingsViewModel.saveEnableNotifications(it.currentValue.value) }
+            ),
+            OptionData.InputOptionData(
+                optionTitle = "Anger detection time in seconds before trigger notification",
+                defaultValue = settingsViewModel.angerDetectionTime.first(),
+                onValidate = { Validation.isNumberInRange(it, 1, 60) },
+                onSave = { settingsViewModel.saveAngerDetectionTime(it.currentValue.value) }
+            )
         )
-    )
-
+    }
     ReturnScaffold(
         viewName = stringResource(R.string.nav_settings),
         onReturn = { navController.popBackStack() }
@@ -61,7 +66,7 @@ fun SettingsView(
                 .fillMaxHeight()
                 .background(color = MaterialTheme.colorScheme.background)
         ) {
-            items(data) {
+            items(data.value) {
                 it.SettingsOption()
             }
         }
