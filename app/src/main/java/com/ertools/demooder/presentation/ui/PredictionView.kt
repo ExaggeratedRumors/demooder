b52454e8e8a8e56f2e49bc1c00834e22ce60b228
ExaggeratedRumors <dgajda.it@gmail.com>
2025-05-10 23:28:42 +0200

    animation

--- app/src/main/java/com/ertools/demooder/presentation/ui/PredictionView.kt
+++ app/src/main/java/com/ertools/demooder/presentation/ui/PredictionView.kt
@@ -1,9 +1,7 @@
 package com.ertools.demooder.presentation.ui
 
-import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
@@ -32,9 +30,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -55,7 +51,6 @@ import com.ertools.demooder.presentation.viewmodel.RecorderViewModel
 import com.ertools.demooder.presentation.viewmodel.RecorderViewModelFactory
 import com.ertools.demooder.presentation.viewmodel.SettingsViewModel
 import com.ertools.demooder.utils.AppConstants
-import kotlinx.coroutines.delay
 import java.util.Locale
 import kotlin.math.roundToInt
 
