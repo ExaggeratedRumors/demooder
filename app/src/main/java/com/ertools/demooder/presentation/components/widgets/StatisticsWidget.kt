5cbc0f42b2049365adccd97cb82b73c158dc3d6d
ExaggeratedRumors <dgajda.it@gmail.com>
2025-08-31 21:50:02 +0200

    statistics

---
+++ app/src/main/java/com/ertools/demooder/presentation/components/widgets/StatisticsWidget.kt
@@ -0,0 +1,9 @@
+package com.ertools.demooder.presentation.components.widgets
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StatisticsWidget(modifier: Modifier = Modifier) {
+
+}
